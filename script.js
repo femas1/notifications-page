d1aa50316bf882fa4cc7fbf1aa9730b3f200d214
femas1 <federico.massara2@gmail.com>
2022-10-17 18:41:33 +0200

    added js logic for read notifications

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-'use strict'
+'use strict';
+
+const notificationDot = document.querySelectorAll('.notificationDot');
+
+notificationDot.forEach((dot)=> {
+    dot.addEventListener('click', () => {
+        dot.classList.add('hidden')
+    })
+})
