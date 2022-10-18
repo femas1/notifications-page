47f0e229811138eeb4612775262e8f9a8211c6c3
femas1 <federico.massara2@gmail.com>
2022-10-18 18:42:38 +0200

    js logic change color notificationcount

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 const notificationDots = document.querySelectorAll('.notificationDot');
 const markRead = document.querySelector('.container__header__mark');
 const notificationNum = document.querySelector('.number');
+const notificationSymbol = document.querySelector('.container__header__new')
 let notificationCount = 3;
 
 
@@ -12,10 +13,17 @@ notificationDots.forEach((dot)=> {
         dot.classList.add('hidden')
         notificationCount--;
         notificationNum.innerText = notificationCount;
+        console.log(notificationCount)
+        if(notificationCount === 0){
+            notificationSymbol.style.backgroundColor = "hsl(219, 85%, 26%)";
+        }
     })
 })
 
 markRead.addEventListener('click', () => {
     notificationDots.forEach(notification => notification.classList.add('hidden'));
     notificationNum.innerText = 0;
+    notificationCount = 0;
+    notificationSymbol.style.backgroundColor = "hsl(219, 85%, 26%)"
 })
+
